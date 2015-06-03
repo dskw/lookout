a914cee0d17e6c86a6c3a69734b2ed238afc748a
dskw <andreas-skowron@dskw.de>
2015-06-03 22:00:41 +0200

    trying to fix the freeze problem after 4+ days runtime

--- lookout.py
+++ lookout.py
@@ -10,22 +10,22 @@ from multiprocessing import Process, Manager
 import platform, subprocess
 
 if getattr(sys, 'frozen', None):
-	static = os.path.join(sys._MEIPASS, 'static')
+    static = os.path.join(sys._MEIPASS, 'static')
 else:
-	static = 'static'
+    static = 'static'
 
 app = flask.Flask(__name__, static_folder=static)
 
 @app.route('/')
 def frontend():
-	return flask.send_from_directory(app.static_folder, 'index.html')
+    return flask.send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/raw')
 def raw():
-	return flask.Response(
-		json.dumps(stats.copy()),
-		mimetype='application/json'
-	)
+    return flask.Response(
+        json.dumps(stats.copy()),
+        mimetype='application/json'
+    )
 
 def get_num_updates():
     avail = 0
@@ -46,34 +46,37 @@ def get_req_reboot():
     return 0
 
 def update(stats):
-	while True:
-		diskused = 0
-		disktotal = 0
-		for i in psutil.disk_partitions():
-			try:
-				x = psutil.disk_usage(i.mountpoint)
-				diskused += x.used
-				disktotal += x.total
-			except OSError:
-				pass
-		stats['uptime'] = time.time() - psutil.BOOT_TIME
-		stats['fqdn'] = socket.getfqdn()
-		stats['cpuusage'] = psutil.cpu_percent(0)
-		stats['ramusage'] = psutil.virtual_memory()
-		stats['diskio'] = psutil.disk_io_counters()
-		stats['diskusage'] = [diskused, disktotal]
-		stats['netio'] = psutil.net_io_counters()
-		stats['swapusage'] = psutil.swap_memory()
-		stats['updates'] = get_num_updates()
-		stats['reboot'] = get_req_reboot()
-		time.sleep(1)
-		
+    while True:
+        try:
+            diskused = 0
+            disktotal = 0
+            for i in psutil.disk_partitions():
+                try:
+                    x = psutil.disk_usage(i.mountpoint)
+                    diskused += x.used
+                    disktotal += x.total
+                except OSError:
+                    pass
+            stats['uptime'] = time.time() - psutil.BOOT_TIME
+            stats['fqdn'] = socket.getfqdn()
+            stats['cpuusage'] = psutil.cpu_percent(0.1, False)
+            stats['ramusage'] = psutil.virtual_memory()
+            stats['diskio'] = psutil.disk_io_counters()
+            stats['diskusage'] = [diskused, disktotal]
+            stats['netio'] = psutil.net_io_counters()
+            stats['swapusage'] = psutil.swap_memory()
+            stats['updates'] = get_num_updates()
+            stats['reboot'] = get_req_reboot()
+            time.sleep(1)
+        except:
+            pass
+        
 if __name__ == '__main__':
-	stats = Manager().dict()
-	Process(target=update, args=(stats, )).start()
-	if len(sys.argv) > 1:
-		PORT = sys.argv[1]
-	server = HTTPServer(WSGIContainer(app))
-	print 'Now listening on port ' + str(PORT)
-	server.listen(PORT)
-	IOLoop.instance().start()
+    stats = Manager().dict()
+    Process(target=update, args=(stats, )).start()
+    if len(sys.argv) > 1:
+        PORT = sys.argv[1]
+    server = HTTPServer(WSGIContainer(app))
+    print 'Now listening on port ' + str(PORT)
+    server.listen(PORT)
+    IOLoop.instance().start()
